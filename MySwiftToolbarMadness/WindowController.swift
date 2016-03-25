cb5aac5718118a5cea87e235c8085a3d2e08b1d1
Nikolai Botev <nikolai_botev@yahoo.com>
2016-03-24 22:27:21 -0700

    Added WindowController class

---
+++ MySwiftToolbarMadness/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  MySwiftToolbarMadness
+//
+//  Created by Nikolay Botev on 3/24/16.
+//  Copyright © 2016 Nikolay Botev. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
